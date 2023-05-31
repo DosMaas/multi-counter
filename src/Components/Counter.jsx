f0ea8c9c8283edcc8eb732823879b19ecae0f442
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-05-30 21:06:54 -0500

    completed one stretch goal

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -10,14 +10,20 @@ function Label(props) {
 
 export function Counter() {
     const [count, setCount] = useState(0);
+
     const increment = () => {
         setCount( count + 1);
     };
-    
+
+    const decrement = () => {
+        setCount( count - 1);
+    }
+
     return (
         <div>
             <Label count = {count} />
             <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 
